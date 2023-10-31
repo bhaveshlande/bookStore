618df403c5563047c2cb719b01c740475fb3abab
bhaveshlande <bhavesh.baliram-lande@capgemini.com>
2023-10-31 17:08:45 +0530

    few issues of netlify are resolved

--- src/Routes/AllRoutes.js
+++ src/Routes/AllRoutes.js
@@ -1,11 +1,21 @@
-import { Routes, Route } from "react-router-dom";
-import { HomePage, ProductsList, ProductDetail, Login, Register, CartPage, OrderPage, DashboardPage, PageNotFound } from "../pages";
+import { Route, Routes } from "react-router-dom";
+import {
+  CartPage,
+  DashboardPage,
+  HomePage,
+  Login,
+  OrderPage,
+  PageNotFound,
+  ProductDetail,
+  ProductsList,
+  Register,
+} from "../pages";
 import { ProtectedRoute } from "./ProtectedRoute";
 
 export const AllRoutes = () => {
   return (
     <>
-    <Routes>
+      <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="products" element={<ProductsList />} />
         <Route path="products/:id" element={<ProductDetail />} />
@@ -13,12 +23,33 @@ export const AllRoutes = () => {
         <Route path="login" element={<Login />} />
         <Route path="register" element={<Register />} />
 
-        <Route path="cart" element={<ProtectedRoute><CartPage /></ProtectedRoute>} />
-        <Route path="order-summary" element={<ProtectedRoute><OrderPage /></ProtectedRoute>} />
-        <Route path="dashboard" element={<ProtectedRoute><DashboardPage /></ProtectedRoute>} />
+        <Route
+          path="cart"
+          element={
+            <ProtectedRoute>
+              <CartPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="order-summary"
+          element={
+            <ProtectedRoute>
+              <OrderPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="dashboard"
+          element={
+            <ProtectedRoute>
+              <DashboardPage />
+            </ProtectedRoute>
+          }
+        />
 
         <Route path="*" element={<PageNotFound />} />
-    </Routes>
+      </Routes>
     </>
-  )
-}
+  );
+};
