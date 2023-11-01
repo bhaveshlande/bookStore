f9cb2fed4c9c746a22a94de22aedf7c4a94ff4b3
bhaveshlande <bhavesh.baliram-lande@capgemini.com>
2023-11-01 09:56:42 +0530

    footer link to href,link account added

--- src/components/Layouts/Footer.js
+++ src/components/Layouts/Footer.js
@@ -1,19 +1,17 @@
-import { Link } from "react-router-dom";
-
 export const Footer = () => {
   return (
     <footer className="bg-white shadow dark:bg-gray-800">
       <div className="p-4 mx-auto max-w-screen-xl md:flex md:items-center md:justify-between md:p-6">
         <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
           © 2030{" "}
-          <Link to="/" className="hover:underline">
+          <a href="/" className="hover:underline">
             CodeBook
-          </Link>
+          </a>
           . All Rights Reserved.
         </span>
         <div className="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
-          <Link
-            to="/"
+          <a
+            href="https://www.instagram.com/bhaveshbl9697/"
             target="_blank"
             rel="noreferrer"
             className="text-gray-500 hover:text-gray-900 dark:hover:text-white"
@@ -31,9 +29,9 @@ export const Footer = () => {
               />
             </svg>
             <span className="sr-only">Instagram page</span>
-          </Link>
-          <Link
-            to="/"
+          </a>
+          <a
+            href="https://twitter.com/bhaveshbl9697"
             target="_blank"
             rel="noreferrer"
             className="text-gray-500 hover:text-gray-900 dark:hover:text-white"
@@ -47,9 +45,9 @@ export const Footer = () => {
               <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
             </svg>
             <span className="sr-only">Twitter page</span>
-          </Link>
-          <Link
-            to="/"
+          </a>
+          <a
+            href="https://github.com/bhaveshbl9697?tab=repositories/"
             target="_blank"
             rel="noreferrer"
             className="text-gray-500 hover:text-gray-900 dark:hover:text-white"
@@ -67,7 +65,25 @@ export const Footer = () => {
               />
             </svg>
             <span className="sr-only">GitHub account</span>
-          </Link>
+          </a>
+          <a
+            href="https://www.linkedin.com/in/bhavesh-lande-630892141/"
+            target="_blank"
+            rel="noreferrer"
+            className="text-gray-500 hover:text-gray-900 dark:hover:text-white"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-linkedin"
+              viewBox="0 0 16 16"
+            >
+              <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z" />
+            </svg>
+            <span className="sr-only">LinkedIn account</span>
+          </a>
         </div>
       </div>
     </footer>
